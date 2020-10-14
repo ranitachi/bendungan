7bd9b68e4d7d09d65400bda87d3e06f3ee045b3c
ranitachi <fachran.nazarullah@gmail.com>
2020-10-14 09:22:28 +0700

    update

--- resources/views/vendor/backpack/base/includes/footer.blade.php
+++ resources/views/vendor/backpack/base/includes/footer.blade.php
@@ -4,12 +4,12 @@
         <!--begin::Copyright-->
         <div class="text-dark order-2 order-md-1">
             <span class="text-muted font-weight-bold mr-2">2020 ©</span>
-            <a href="http://sysable.io" target="_blank" class="text-dark-75 text-hover-primary">Powered By Sysable Tekologi Indonesia</a>
+            <a href="http://aurumsystem.com" target="_blank" class="text-dark-75 text-hover-primary">Powered By Aurum System</a>
         </div>
         <!--end::Copyright-->
         <!--begin::Nav-->
         <div class="nav nav-dark order-1 order-md-2">
-            <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pr-3 pl-0">Visit Sysable</a>
+            <a href="aurumsystem.com" target="_blank" class="nav-link pr-3 pl-0">Visit Aurum System</a>
         </div>
         <!--end::Nav-->
     </div>
